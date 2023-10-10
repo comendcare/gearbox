bd6750145f7d942f6ee445cfc6af256b6c2393f4
flawnson <flawnsontong1@gmail.com>
2023-10-10 13:35:30 -0400

    Successfully created fine tuning files and script

--- fine_tuning/fine_tune_openai.py
+++ fine_tuning/fine_tune_openai.py
@@ -1,9 +1,20 @@
+"""Fine tuning script for openai models"""
 import os
 import openai
-openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.File.create(
-  file=open("mydata.jsonl", "rb"),
-  purpose='fine-tune'
-)
+openai.api_key = "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"
+# response = openai.File.create(
+#   file=open("../data/fine_tune.jsonl", "rb"),
+#   purpose='fine-tune'
+# )
 
-openai.FineTuningJob.create(training_file="intralingual-fine-tune", model="gpt-3.5-turbo")
+# print(response)
+# print(openai.File.delete(sid="file-bwUke2apOlqkhZUYbNxaj3BI"))
+print(openai.File.list(limit=10))
+
+# openai.FineTuningJob.create(training_file="file-M3U1oEY0FkbwGCUuMZU9tKnF", model="gpt-3.5-turbo")
+
+# print(openai.FineTuningJob.cancel("ftjob-CPOaqHjG6fobJ6CgaZ3Lx5k5"))
+print(openai.FineTuningJob.retrieve("ftjob-Rq2GW2KBMdPFkiddAHmXpll7"))
+# print(openai.FineTuningJob.list(limit=10))
+print(openai.FineTuningJob.list_events(id="ftjob-Rq2GW2KBMdPFkiddAHmXpll7", limit=10))
+
